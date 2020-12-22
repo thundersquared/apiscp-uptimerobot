416ae08f3943606a70b8a029f76b8a2f1edba7d3
Anatoli Nicolae <an@thundersquared.com>
2020-12-22 12:10:29 +0100

    Fix hook
    Signed-off-by: Anatoli Nicolae <an@thundersquared.com>

--- src/sqrd/Composer/Hooks.php
+++ src/sqrd/Composer/Hooks.php
@@ -6,12 +6,20 @@ use Composer\Script\Event;
 
 class Hooks
 {
-    const APISDIR = '/usr/local/apnscp/config/custom';
+    const APISDIR = '/usr/local/apnscp';
+    const APISCSTM = '/usr/local/apnscp/config/custom';
     public static function postUpdate(Event $event) : void
     {
-        mkdir(static::APISDIR . '/apps/login/views/partials/', 755, true);
-        copy('src/sqrd/ApisCP/Extensions/views/status.blade.php', static::APISDIR . '/apps/login/views/partials/status.blade.php');
-        copy('src/sqrd/ApisCP/Extensions/UptimeRobot.php', static::APISDIR . '/UptimeRobot.php');
+        if (is_dir(static::APISCSTM . '/apps/login/views/partials/'))
+        {
+            mkdir(static::APISCSTM . '/apps/login/views/partials/', 755, true);
+        }
+
+        copy('src/sqrd/ApisCP/Extensions/views/status.blade.php', static::APISCSTM . '/apps/login/views/partials/status.blade.php');
+        copy('src/sqrd/ApisCP/Extensions/UptimeRobot.php', static::APISCSTM . '/UptimeRobot.php');
+
+        exec(sprintf('cd %s ; composer dumpautoload -o', static::APISDIR));
+
         echo "You're done!";
     }
 }
